eb0c897d752b4d805572f36d9d4d6cafbbc59b20
Diego López <lopez.diego@cetys.edu.mx>
2024-11-13 09:11:55 -0800

    chakra

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+//import { Flex, Image, Text } from '@chakra-ui/react';
+
+const Home = () => {
+    return (
+        <div>
+            Ramiro gei
+        </div>
+    );
+};
+
+export default Home;
