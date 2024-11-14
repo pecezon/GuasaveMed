ca9b79d4f27d90053dc68a226e5e2b1ead4e85e0
Diego López <lopez.diego@cetys.edu.mx>
2024-11-13 20:47:36 -0800

    Cat Test

--- guasavemedapi/src/main/java/io/finalAMS/guasavemedapi/repository/CatRepository.java
+++
@@ -1,9 +0,0 @@
-package io.finalAMS.guasavemedapi.repository;
-
-import io.finalAMS.guasavemedapi.model.Cat;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CatRepository extends JpaRepository<Cat, Long> {
-}
