2128bddcd683f28bd483525560116c4d0ecdb1f4
diegopez <lopez.diego@cetys.edu.mx>
2024-11-23 12:00:09 -0800

    Crear Paciente Realizado

--- guasavemedapi/src/main/java/io/finalAMS/guasavemedapi/Controllers/PacienteController.java
+++ guasavemedapi/src/main/java/io/finalAMS/guasavemedapi/Controllers/PacienteController.java
@@ -1,9 +1,31 @@
 package io.finalAMS.guasavemedapi.Controllers;
 
+import io.finalAMS.guasavemedapi.Models.Gato;
+import io.finalAMS.guasavemedapi.Models.Paciente;
+import io.finalAMS.guasavemedapi.Services.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/paciente")
 public class PacienteController {
+
+    private PacienteService pacienteService;
+
+    @Autowired
+    public PacienteController(PacienteService pacienteService){
+        this.pacienteService = pacienteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Paciente> crearPaciente(@RequestBody Paciente paciente) {
+        Paciente pacienteGuardado = pacienteService.crearPaciente(paciente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(pacienteGuardado);
+    }
 }
--- guasavemedapi/src/main/java/io/finalAMS/guasavemedapi/Services/PacienteService.java
+++ guasavemedapi/src/main/java/io/finalAMS/guasavemedapi/Services/PacienteService.java
@@ -1,7 +1,22 @@
 package io.finalAMS.guasavemedapi.Services;
 
+import io.finalAMS.guasavemedapi.Models.Paciente;
+import io.finalAMS.guasavemedapi.Repository.GatoRepository;
+import io.finalAMS.guasavemedapi.Repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PacienteService {
+
+    private PacienteRepository pacienteRepository;
+
+    @Autowired
+    public PacienteService(PacienteRepository pacienteRepository){
+        this.pacienteRepository = pacienteRepository;
+    }
+
+    public Paciente crearPaciente(Paciente paciente){
+        return pacienteRepository.save(paciente);
+    }
 }
