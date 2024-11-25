009fd6261cf266ad36feaf30ce6f05ac45d0c785
diegopez <lopez.diego@cetys.edu.mx>
2024-11-24 16:19:51 -0800

    Backend Terminado?, PUTS y arreglos

--- guasavemedapi/src/main/java/io/finalAMS/guasavemedapi/Controllers/PacienteController.java
+++ guasavemedapi/src/main/java/io/finalAMS/guasavemedapi/Controllers/PacienteController.java
@@ -1,12 +1,14 @@
 package io.finalAMS.guasavemedapi.Controllers;
 
 import io.finalAMS.guasavemedapi.Models.Paciente;
+import io.finalAMS.guasavemedapi.Models.Paciente;
 import io.finalAMS.guasavemedapi.Services.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -20,16 +22,37 @@ public class PacienteController {
         this.pacienteService = pacienteService;
     }
 
+    //Crear Paciente
     @PostMapping
     public ResponseEntity<Paciente> crearPaciente(@RequestBody Paciente paciente) {
         Paciente pacienteGuardado = pacienteService.crearPaciente(paciente);
         return ResponseEntity.status(HttpStatus.CREATED).body(pacienteGuardado);
     }
 
+    //Obtener Paciente
     @GetMapping("/{id}")
     public ResponseEntity<Paciente> obtenerPacientePorId(@PathVariable Long id) {
         Optional<Paciente> paciente = pacienteService.obtenerPacientePorId(id);
         return paciente.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
+
+    //Modificar Paciente
+    @PutMapping("/{id}")
+    public ResponseEntity<Paciente> modificarPaciente(@PathVariable Long id, @RequestBody Paciente pacienteActualizado) {
+        try {
+            Paciente paciente = pacienteService.modificarPaciente(id, pacienteActualizado);
+            return ResponseEntity.ok(paciente);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    //Obtener Todos Los Pacientes motivos de testing
+    @GetMapping
+    public List<Paciente> obtenerPacientes(){
+        return pacienteService.obtenerPacientes();
+    }
+
+
 }
--- guasavemedapi/src/main/java/io/finalAMS/guasavemedapi/Services/PacienteService.java
+++ guasavemedapi/src/main/java/io/finalAMS/guasavemedapi/Services/PacienteService.java
@@ -1,11 +1,13 @@
 package io.finalAMS.guasavemedapi.Services;
 
+import io.finalAMS.guasavemedapi.Models.Gato;
 import io.finalAMS.guasavemedapi.Models.Paciente;
 import io.finalAMS.guasavemedapi.Repository.GatoRepository;
 import io.finalAMS.guasavemedapi.Repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,7 +23,22 @@ public class PacienteService {
     public Paciente crearPaciente(Paciente paciente){
         return pacienteRepository.save(paciente);
     }
+
     public Optional<Paciente> obtenerPacientePorId(long id){
         return pacienteRepository.findById(id);
+    }
+
+    public List<Paciente> obtenerPacientes(){
+        return pacienteRepository.findAll();
+    }
+
+    // Método para actualizar un paciente
+    public Paciente modificarPaciente(Long id, Paciente pacienteModificado) {
+        return pacienteRepository.findById(id).map(paciente -> {
+            paciente.setNombre(pacienteModificado.getNombre());
+            paciente.setEdad(pacienteModificado.getEdad());
+            paciente.setTelefono(pacienteModificado.getTelefono());
+            return pacienteRepository.save(paciente);
+        }).orElseThrow(() -> new RuntimeException("Paciente no encontrado"));
     }
 }
